caa451cc7716cf3d31ea74be67adaa48a2bb9019
Genesis Oghoanina <goghoanina@gmail.com>
2023-06-20 10:52:06 +0100

    second commit

---
+++ shellscript_tutorial/myscript.sh
@@ -0,0 +1,60 @@
+ #! /bin/bash
+
+ # ECHO COMMAND
+ echo Hello World!
+
+ # VARIABLES
+ # Uppercase by convention
+# Letters, numbers, underscores
+NAME="Nina"
+# echo "My name is $NAME"
+# echo "My name is ${NAME}"
+
+# USER INPUT 
+# read -p "Enter your name: " NAME
+# echo "Hello $NAME, nice to meet you!"
+
+# SIMPLE IF STATEMENT
+# if [ "$NAME" == "Genesis" ]
+# then
+#     echo "Your name is Genesis"
+# fi
+
+# IF-ELSE
+# if [ "$NAME" == "Genesis" ]
+# then
+#     echo "Your name is Genesis"
+# else
+#     echo "Your name is NOT Genesis"
+# fi
+
+# ELSE-IF (elif)
+# if [ "$NAME" == "Genesis" ]
+# then
+#     echo "Your name is Genesis"
+# elif [ "$NAME" == "Royal" ]
+# then
+#     echo "Your name is Royal"
+# else
+#     echo "Your name is NOT Genesis or Royal"
+# fi
+
+########
+# -eq if valuse are equal
+# -ne  values are not equal
+# -gt  greater than 
+# -ge greater than or equal to
+# -lt less than
+# -le less  than or equal to
+
+# COMPARISON
+# NUM1=31
+# NUM2=5
+# if [ "$NUM1" -gt "$NUM2" ]
+# then
+#     echo "$NUM1 is greater than $NUM2"
+# else
+#     echo "$NUM1 is less than $NUM2"
+# fi
+
+# FILE CONDITIONS
